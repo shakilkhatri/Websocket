662d20e2fb9d2c8b85ff4b107bf20ece60b628a9
shakilkhatri <43198367+shakilkhatri@users.noreply.github.com>
2023-09-09 19:09:24 +0530

    Create server Index.js

---
+++ Server/Index.js
@@ -0,0 +1,28 @@
+const WebSocket = require("ws");
+
+// Initialize the server
+const wss = new WebSocket.Server({ port: 3000 });
+
+// Array to store connected clients
+const clients = [];
+
+// Function to broadcast the text changes to all connected clients
+function broadcastChanges(message) {
+  clients.forEach((client) => client.send(message));
+}
+
+// WebSocket server event listeners
+wss.on("connection", function connection(ws) {
+  clients.push(ws);
+
+  ws.on("message", function incoming(message) {
+    broadcastChanges(message);
+  });
+
+  ws.on("close", function () {
+    const index = clients.indexOf(ws);
+    if (index !== -1) {
+      clients.splice(index, 1);
+    }
+  });
+});
